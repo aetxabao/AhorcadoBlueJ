94d82199c5833bac50bb153ef7813cf6f8eee88d
aetxabao <aitor.etxabarren@gmail.com>
2021-09-30 12:18:44 +0200

    Entrada

---
+++ Diccionario.java
@@ -0,0 +1,15 @@
+/**
+ * Clase para generar palabras aleatorias.
+ */
+public class Diccionario {
+    
+   /**
+    * Devuelve una palabra aleatoria.
+    * @return palabra aleatoria
+    */
+   public String creaPalabraAleatoria(){
+       //TODO: Crear una palabra aleatoria, ahora devuelve "aleatorio"
+       return "aleatorio";
+   }
+   
+}
